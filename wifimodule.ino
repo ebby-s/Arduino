a04b9567e4581f97aad7087913b6ae53586c158c
ebby-s <31300178+ebby-s@users.noreply.github.com>
2017-08-24 20:36:50 +0800

    Add files via upload

---
+++ wifimodule.ino
@@ -0,0 +1,39 @@
+#include <SoftwareSerial.h>
+   
+const int RX1 = 11;
+const int TX1 = 10;
+
+
+
+// create SoftwareSerial objects
+SoftwareSerial SoftSerialOne(RX1,TX1);
+
+   void setup()
+   {
+     pinMode(RX1, INPUT);
+     pinMode(TX1, OUTPUT);
+
+     Serial.begin(9600);
+     SoftSerialOne.begin(9600);
+     Serial.println("Test Start");
+     SoftSerialOne.println("AT+CWJAP=\"SSID\",\"password\"");
+     Serial.setTimeout(5000);
+    
+   }
+   
+   void loop()
+   {
+     
+     while(Serial.available())
+     {
+       
+       SoftSerialOne.write(Serial.read());
+     }
+     while(SoftSerialOne.available())
+     {
+       Serial.write(SoftSerialOne.read());
+       Serial.setTimeout(5000);
+     }
+     
+    
+   }
