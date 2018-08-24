cb1e8f5cacb664594909697b3372d0de55f1da9e
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-08-24 09:32:22 +0800

    Add files via upload

---
+++ sensors/sensors.ino
@@ -0,0 +1,42 @@
+ #include <dht.h>
+dht DHT;
+#define DHT11_PIN 7
+
+int sensor = 2;
+int trigger = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(sensor, INPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration, distance;
+  digitalWrite(trigger, LOW);
+  delay(2);
+  digitalWrite(trigger, HIGH);
+  delay(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(sensor, HIGH);
+  distance = duration/58.2;
+
+  char buf[16];
+  String strval = dtostrf(distance, 4, 1, buf);
+
+  Serial.print("Distance:");
+  Serial.println(strval);
+
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature: ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity: ");
+  Serial.println(DHT.humidity);
+
+  delay(1000);
+
+}
---
+++ wave62/wave62.ino
@@ -0,0 +1,24 @@
+int leds[] = {3,5,6,9,10,11};
+int pwm_lvls[] = {3,4,5,6,7,8};
+float change = 0.0625;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i=0;i<=5;i++){
+    pwm_lvls[i]-=change;
+    if (pwm_lvls[i]<=0){
+      pwm_lvls[i] = 7.99 + pwm_lvls[i];
+    }
+  }
+  for (int i=0;i<=5;i++){
+    analogWrite(leds[i],pow(2,pwm_lvls[i]));
+  }
+  delay(60);
+
+}
